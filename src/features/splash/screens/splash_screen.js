befafb33acb6b6e63b27eb6a11127f6855060ea2
tama <pratama.sa@meteor.id>
2020-07-05 16:21:25 +0700

    add react antive config

--- src/features/splash/screens/splash_screen.js
+++ src/features/splash/screens/splash_screen.js
@@ -1,63 +1,71 @@
 import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
-import { StackActions, CommonActions } from '@react-navigation/native';
+import { StackActions } from '@react-navigation/native'
+import Config from 'react-native-config'
 
 import { connect } from 'react-redux'
 import { ScreenName } from '../../../utils/constan'
 
-
 const SplashScreen = ({ navigation, todos, add }) => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green' }}>
-            <TouchableOpacity onPress={() => {
-                // navigation.navigate(ScreenName.onboardScreen)
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green'
+      }}>
+      <TouchableOpacity
+        onPress={() => {
+          // navigation.navigate(ScreenName.onboardScreen)
 
-                /**
-                 * REPLACE EXAMPLE
-                 */
-                navigation.dispatch(
-                    StackActions.replace(ScreenName.onboardScreen, {
-                        // user: 'jane',
-                    })
-                );
+          /**
+           * REPLACE EXAMPLE
+           */
+          navigation.dispatch(
+            StackActions.replace(ScreenName.onboardScreen, {
+              // user: 'jane',
+            })
+          )
 
+          /**
+           * RESET EXAMPLE
+           */
+          // navigation.reset({
+          //     index: 1,
+          //     routes: [
+          //         {
+          //             name: ScreenName.dashboardScreen,
+          //             params: { user: 'jane', screen: ScreenName.transactionStack },
+          //         },
+          //         {
+          //             name: ScreenName.loginScreen,
+          //             // params: { user: 'jane', screen: ScreenName.transactionStack },
+          //         },
+          //     ],
+          // })
 
-
-                /**
-                 * RESET EXAMPLE
-                 */
-                // navigation.reset({
-                //     index: 1,
-                //     routes: [
-                //         {
-                //             name: ScreenName.dashboardScreen,
-                //             params: { user: 'jane', screen: ScreenName.transactionStack },
-                //         },
-                //         {
-                //             name: ScreenName.loginScreen,
-                //             // params: { user: 'jane', screen: ScreenName.transactionStack },
-                //         },
-                //     ],
-                // })
-
-                // add()
-            }}>
-                <Text>Splash screen {todos.length}</Text>
-            </TouchableOpacity>
-        </View>
-    )
+          // add()
+        }}>
+        <Text>Splash screen {Config.BASE_API_URL}</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-        todos: state.dummyReducer.todos
-    }
+  return {
+    todos: state.dummyReducer.todos
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        add: () => dispatch({ type: 'Add' })
-    }
+  return {
+    add: () => dispatch({ type: 'Add' })
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SplashScreen)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SplashScreen)

4824a8ff786f2ac42be824e353da02927e6496d0
tama <pratama.sa@meteor.id>
2020-07-12 17:51:22 +0700

    add rn fast image library

--- src/features/onboard/screens/onboard_screen.js
+++ src/features/onboard/screens/onboard_screen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import FastImage from 'react-native-fast-image'
 
 const OnBoardScreen = () => {
   return (
@@ -10,6 +11,15 @@ const OnBoardScreen = () => {
         }}>
         <Text>Onboard screen</Text>
       </TouchableOpacity>
+      <FastImage
+        style={{ width: 200, height: 200 }}
+        source={{
+          uri: 'https://unsplash.it/400/400?image=1',
+          headers: { Authorization: 'someAuthToken' },
+          priority: FastImage.priority.normal
+        }}
+        resizeMode={FastImage.resizeMode.contain}
+      />
     </View>
   )
 }
